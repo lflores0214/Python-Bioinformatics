3e5bc8c241643286bd3d9ed7d2aad5fc28f5fa79
Luis <lflores02142@gmail.com>
2021-04-16 16:49:19 -0700

    added transcription and reverse compliment problems

--- dna_toolset/dna_toolkit.py
+++ dna_toolset/dna_toolkit.py
@@ -1,4 +1,5 @@
-nucleotides = ["A", "T", "C", "G"]
+from structures import nucleotides, dna_reverse_compliment
+from utilities import colored
 
 
 def validate_sequence(dna_seq):
@@ -18,3 +19,13 @@ def count_nucleotides(dna_seq):
         else:
             nuc_count[nuc] = 1
     return nuc_count
+
+
+def transcription(seq):
+    """DNA -> RNA Transcription. Replacing Thymine with Uracil"""
+    return seq.replace("T", "U")
+
+
+def reverse_compliment(seq):
+    """ Swapping adenine with thymine and guanine with cytosine. Reversing newly generated string """
+    return ''.join([dna_reverse_compliment[nuc] for nuc in seq])[::-1]
