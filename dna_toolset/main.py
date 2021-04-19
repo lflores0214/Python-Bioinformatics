2f18491fda64fe7c1cc15c13663dd19c9fe05359
Luis <lflores02142@gmail.com>
2021-04-19 15:22:28 -0700

    restructured the print statements of main.py

--- dna_toolset/main.py
+++ dna_toolset/main.py
@@ -1,5 +1,5 @@
 # DNA Toolkit/Code testing file
-from dna_toolkit import transcription, nucleotides, validate_sequence, count_nucleotides, reverse_compliment
+from dna_toolkit import transcription, nucleotides, validate_sequence, count_nucleotides, reverse_compliment, gc_content, gc_content_subsection
 from utilities import colored
 import random
 
@@ -13,4 +13,11 @@ print(f"\nSequence: {colored(DNA_STR)}")
 print(f"[1] + Sequence Length: {len(DNA_STR)} \n")
 print(f"[2] + Nucleotide Frequency  : {count_nucleotides(DNA_STR)} \n")
 print(f"[3] + DNA/RNA Transcription : {colored(transcription(DNA_STR))}\n")
-print(f"[4] + Reverse Compliment    : {colored(reverse_compliment(DNA_STR))}\n")
+print(
+    f"[4] + DNA string + Compliment + Reverse Compliment: \n5' {colored(reverse_compliment(DNA_STR))} 3'")
+print(f"3' {colored(reverse_compliment(DNA_STR)[::-1])} 5' [Compliment]")
+print(f"5' {colored(reverse_compliment(DNA_STR))} 3' [Reverse Compliment]")
+print(f"[5] + GC Content : {gc_content(DNA_STR)}%")
+print(f"[6] + GC Content in subsection k=5 : {gc_content_subsection(DNA_STR, k=5)}\n")
+
+
